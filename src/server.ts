9e0ed2874cebd59307b86c6b7625b832824c0ba3
jchris3434 <jc.fontaine34@gmail.com>
2024-02-14 09:42:45 +0100

    containers works ok!

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -7,8 +7,8 @@ const port = 3000;
 // Connecting to the MongoDB database
 mongoose.connect('mongodb://localhost/jcdb').then(() => {
     console.log('Connexion OK a MongoDB');
-}).catch((err:any) => {
-    console.log('Connexion Failed a MongoDB', err); // Logging an error message if the connection fails
+}).catch((err) => {
+    console.log('Connexion Failed a MongoDB', err); 
 });
 
 app.get('/', (req, res) => {
@@ -19,4 +19,4 @@ app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
 
-export default app;
+module.exports = app;
